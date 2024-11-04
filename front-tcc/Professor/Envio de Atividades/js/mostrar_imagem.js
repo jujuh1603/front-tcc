2330af32f65c00c6335e2ff87212ab7054e8a724
Julia Caetano Bellusci <152660693+jujuh1603@users.noreply.github.com>
2024-11-04 20:02:10 -0300

    Add files via upload

--- front-tcc/Professor/Envio de Atividades/js/mostrar_imagem.js
+++ front-tcc/Professor/Envio de Atividades/js/mostrar_imagem.js
@@ -1,8 +1,1 @@
-    // Seleciona o botão e a div
-    const button = document.getElementById("mostrarBtn");
-    const div = document.getElementById("imagemDiv");
 
-    // Adiciona o evento de clique ao botão
-    button.addEventListener("click", function() {
-        div.classList.toggle("show"); // Alterna a classe 'show' para exibir ou ocultar a div
-    });
